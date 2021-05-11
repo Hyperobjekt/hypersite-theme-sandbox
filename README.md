418178c313c988f0f6505a74cd5a829b9edee3a4
lane <laneolson@gmail.com>
2021-05-11 11:54:43 -0700

    Initial commit from gatsby: (https://github.com/Hyperobjekt/gatsby-starter-hypersite.git)

---
+++ README.md
@@ -0,0 +1,11 @@
+# gatsby-starter-hypersite
+
+A starter that uses `gatsby-theme-hypersite` to provide:
+
+- Material UI Components / Theming
+- Page creation
+- SEO
+- MDX content
+- Custom site components
+
+This repository is automatically generated from pushes to: https://github.com/Hyperobjekt/gatsby-themes
---
+++ src/gatsby-theme-hypercore/theme.js
@@ -0,0 +1,42 @@
+import pink from "@material-ui/core/colors/pink"
+import { createMuiTheme } from "@material-ui/core"
+
+let theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#6a00ff",
+    },
+    secondary: {
+      main: pink[500],
+    },
+  },
+  overrides: {
+    MuiCssBaseline: {
+      "@global": {
+        ".HypBranding-root": {
+          color: "#fff",
+          fontSize: 24,
+          textDecoration: "none",
+        },
+        "[class*='Hero-content'] > .MuiTypography-root:first-child": {
+          textShadow: `0 1px 4px #000`,
+        },
+        code: {
+          background: "rgb(45, 42, 85)",
+          color: "rgb(158, 254, 255)",
+          padding: "0 4px",
+          borderRadius: "4px",
+          display: "inline-block",
+        },
+        ".MuiTypography-body1 + .MuiTypography-body1": {
+          marginTop: "1rem",
+        },
+      },
+    },
+    MuiDivider: {
+      root: { margin: "1rem 0" },
+    },
+  },
+})
+
+export default theme
